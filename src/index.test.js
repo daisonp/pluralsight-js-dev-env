321d154e5a6b7590e16b110ffe1979f7f9585652
Richard Parry <richard.w.parry@gmail.com>
2018-02-03 15:12:27 +0000

    Fix broken test

--- src/index.test.js
+++ src/index.test.js
@@ -25,7 +25,7 @@ describe ('index.html', () => {
         const index = fs.readFileSync('./src/index.html',"utf-8");
         jsdom.env(index, function(err,window) {
             const h2 = window.document.getElementsByTagName('h2')[0];
-            expect(h2.innerHTML).to.equal("Some other text!");
+            expect(h2.innerHTML).to.equal("Some other text");
             done();
             window.close();
         });
